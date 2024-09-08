c0bcb90a165b39bf3edc24ce87efb7da17a2b7ca
John Oluwasegun Kehinde <kehindejohn018@gmail.com>
2024-09-08 22:04:27 +0100

    cleaned up code

--- CalculatorChallenge.App/Program.cs
+++ CalculatorChallenge.App/Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-using CalculatorChallenge.App.Implementation;
+﻿using CalculatorChallenge.App.Implementation;
 using CalculatorChallenge.App.Interface;
 using Microsoft.Extensions.DependencyInjection;
 
